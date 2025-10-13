506099d5c8aef9c2061e849767a55dbe0211161c
AzureAD\SandozAnto <s.anto@shearfrac.com>
2025-10-13 17:13:06 -0600

    remove package

--- SpeedTestServer/src/index.js
+++ SpeedTestServer/src/index.js
@@ -1,26 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-const FastSpeedtest = require("fast-speedtest-api");
+const pkg = require("@cloudflare/speedtest");
+const SpeedTest = pkg.default;
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-const speedtest = new FastSpeedtest({
-  token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // example token
-  verbose: false,
-  timeout: 5000,
-  https: true,
-  urlCount: 5,
-  bufferSize: 8,
-  unit: FastSpeedtest.UNITS.Mbps,
-});
+// const speedtest = new FastSpeedtest({
+//   token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // example token
+//   verbose: false,
+//   timeout: 5000,
+//   https: true,
+//   urlCount: 5,
+//   bufferSize: 8,
+//   unit: FastSpeedtest.UNITS.Mbps,
+// });
+
+// app.get("/run", async (req, res) => {
+//   try {
+//     const speed = await speedtest.getSpeed();
+//     res.json({ downloadSpeedMbps: speed });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 app.get("/run", async (req, res) => {
   try {
-    const speed = await speedtest.getSpeed();
-    res.json({ downloadSpeedMbps: speed });
+    const speedTest = new SpeedTest({
+      download: { duration: 15000 }, // 15 s sustained download
+    });
+    speedTest.onFinish = (results) => {
+      const summary = results.getSummary();
+      console.log(summary);
+      res.json({ downloadSpeedMbps: (summary.download / 1250000).toFixed(2) });
+    };
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
